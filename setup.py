32779f645942d00624942c6507e28771aa2dd0ce
Eric Kofman <erickofman@vpn5-78.broadinstitute.org>
2017-11-20 14:19:32 -0500

    One last description update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='deconstructSigs',
-      description='Python implementation of DeconstructSigs algorithm for deducing cancer genome mutational signatures',
+      description='Implementation of DeconstructSigs algorithm for deducing cancer genome mutational signatures',
       author='Eric Kofman',
       author_email='ericrkofman@gmail.com',
       version='1.1',
