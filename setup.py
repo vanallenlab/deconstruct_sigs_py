7ab7676accff225f6dc4d30ee8d0f8cab94a26a1
Eric Kofman <erickofman@vpn5-78.broadinstitute.org>
2017-11-20 14:03:05 -0500

    Add setup py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='deconstructSigs',
+      author='Eric Kofman',
+      author_email='ericrkofman@gmail.com',
+      version='1.0',
+      py_modules=['DeconstructSigs']
+      )
