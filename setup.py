96071e2524cd62be5e9dc2a7a09a0cd67a32729b
Eric Kofman <erickofman@vpn5-78.broadinstitute.org>
2017-11-20 14:15:57 -0500

    Update setup py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(name='deconstructSigs',
       author='Eric Kofman',
       author_email='ericrkofman@gmail.com',
       version='1.1',
-      py_modules=['DeconstructSigs'],
+      py_modules=['deconstructSigs'],
       url='https://github.com/vanallenlab/deconstruct_sigs_py',
       download_url='https://github.com/vanallenlab/deconstruct_sigs_py/archive/v1.1.tar.gz'
       )
