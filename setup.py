d220bab7dc1b09ed841e5af58186c0cb9def40a1
Eric Kofman <ericrkofman@gmail.com>
2017-12-21 12:36:59 -0500

    New changes

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from distutils.core import setup
 
-from setuptools import find_packages
-my_packages=find_packages()
 setup(name='deconstructSigs',
       description='Implementation of DeconstructSigs algorithm for deducing cancer genome mutational signatures',
       author='Eric Kofman',
       author_email='ericrkofman@gmail.com',
-      version='1.46',
-      py_modules=my_packages,
+      version='1.48',
+      py_modules=['deconstructSigs'],
       url='https://github.com/vanallenlab/deconstruct_sigs_py',
-      download_url='https://github.com/vanallenlab/deconstruct_sigs_py/archive/v1.46.tar.gz'
+      download_url='https://github.com/vanallenlab/deconstruct_sigs_py/archive/v1.47.tar.gz'
       )
