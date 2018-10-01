dc558c0c38758d309a6591a18eee2e847cd6d3bb
pwwang <1188067+pwwang@users.noreply.github.com>
2018-10-01 14:39:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
-setup(name='deconstructSigs',
-      description='Implementation of DeconstructSigs algorithm for deducing cancer genome mutational signatures',
-      author='Eric Kofman',
-      author_email='ericrkofman@gmail.com',
-      version='1.48',
-	packages= find_packages(),
-      url='https://github.com/vanallenlab/deconstruct_sigs_py',
-      download_url='https://github.com/vanallenlab/deconstruct_sigs_py/archive/v1.47.tar.gz'
-      )
+setup (
+	name             = 'deconstructSigs',
+	version          = '1.48',
+	description      = "Implementation of DeconstructSigs algorithm for deducing cancer genome mutational signatures",
+	url              = "https://github.com/pwwang/deconstruct_sigs_py",
+	author           = "Eric Kofman; pwwang",
+	author_email     = "ericrkofman@gmail.com",
+	packages         = find_packages()
+)
